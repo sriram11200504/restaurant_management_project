df3f12ae5923416db9ab75156d0db44952f50eac
sriram11200504 <sriramlohith25@gmail.com>
2025-08-06 22:43:46 +0530

    17745

--- products/models.py
+++ products/models.py
@@ -8,3 +8,9 @@ class Item(models.Model):
 
     def __str__(self):
         return str(self.item_name)
+class MenuItem(models.Model):
+    name=models.CharField(max_length=100)
+    description=models.TextField()
+    price=models.DecimalField(max_digits=6,decimal_places=2)
+    def __str__(self):
+        return self.name
