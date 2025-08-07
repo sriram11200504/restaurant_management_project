c13c20a4bab8ab9e0b14a8c497fdbcc5a53a7c4a
sriram11200504 <sriramlohith25@gmail.com>
2025-08-07 22:34:11 +0530

    17746

--- products/views.py
+++ products/views.py
@@ -19,7 +19,7 @@ class ItemView(APIView):
         return Response(serializer.data, status=status.HTTP_200_OK)
 
     def post(self, request):
-        serializer = ItemSerializer(data=request.data)
+        serializer =ItemSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
