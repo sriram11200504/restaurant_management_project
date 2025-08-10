ca1189c0e2ff79ca23b026c0825c401826c8034f
sriram11200504 <sriramlohith25@gmail.com>
2025-08-10 18:02:55 +0530

    27098

--- home/views.py
+++ home/views.py
@@ -1,21 +1,25 @@
 import requests
 from django.shortcuts import render
 from django.conf import settings
-
 def home(request):
     menu_data=[]
     try:
         response=requests.get('http://127.0.0.1:8000/api/products/menu-items/')
-
-        if response.status_code=200:
+        if response.status_code==200:
             menu_data=response.json()
         else:
-            print(f"error fetching menu : {response.status_code}")
+            print(f"error fetching menu: {response.status_code}")
     except requests.exceptions.RequestException as e:
         print(f"failed to connect to API: {e}")
-
+    
     context={
-        'menu-item' = menu_data,
-        'restaurant_name'=settings.RESTAURANT_NAME
+        'menu_items': menu_data,
+        'restaurant_name': settings.RESTAURANT_NAME
     }
     return render(request,'home/menu.html',context)
+def about_view(request):
+    context={
+        'restaurant_name': settings.RESTAURANT_NAME
+    }
+    return render(request,'home/about.html',context)
+
