e410bba3d567e2d6c24f9b316ac695f9914699a2
sriram11200504 <sriramlohith25@gmail.com>
2025-08-12 22:08:32 +0530

    29041

---
+++ home/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+
+class ContactForm(forms.Form):
+    name=forms.CharField(max_length=100,required=False)
+    email=forms.EmailField(required=True)
+    subject=forms.CharField(required=False,max_length=200)
+    message=forms.CharField(width=forms.Textarea,required=True)
