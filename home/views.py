3d544c8fcc4b44e9d4f1961844fb3b8e00613d72
sriram11200504 <sriramlohith25@gmail.com>
2025-08-14 11:13:53 +0530

    31510

--- home/views.py
+++ home/views.py
@@ -44,4 +44,50 @@ def menu_page_view(request):
         
         'search_query':search_query
     }
+def contact_view(request):
+    if request.method=="POST":
+        form=ContactForm(request.POST)
+        if form.is_valid():
+            name=form.cleaned_data['name']
+            email=form.cleaned_data['email']
+            subject=form.cleaned_data['subject']
+            message=form.cleaned_data["message"]
+
+            email_subject=f"contact form submission:{subject}"
+            email_body= f"name: {name if name else 'Ananimous'}\n" \
+                        f"email: {email}\n" \
+                        f"subject: {subject}\n" \
+                        f"message: {message}\n" \
+            try:
+                send_mail(
+                    email_subject,
+                    email_body,
+                    settings.DEFAULT_FROM_EMAIL,
+                    [settings.RECIPEINT_EMAIL],
+                    fail_silently=False,
+                )
+                return render(request,'home/contact_form.html',{
+                    'form':ContactForm(),
+                    'success message': 'your message has sent successfully',
+                    'restaurant_name':settings.RESTAURANT_NAME
+                })
+            except Exception as e:
+                print(f"error sending email: {e}")
+                return render(request,'home/contact_form.html',{
+                    'form':ContactForm(),
+                    'error_message':'there is a error in sending message',
+                    'restaurant_name': settings.RESTAURANT_NAME
+                })
+        else:
+            return render(request,'home/contact_form.html'{
+                'form':ContactForm(),
+                'restaurant_name':settings.RESTAURANT_NAME
+            })
+    else:
+        form=ContactForm()
+        return render(request,'home/contact_form.html',{
+            'form':form,
+            'restaurant_name':settings.RESTAURANT_NAME
+        })
+
     return render(request,'home/menu.html',context)
