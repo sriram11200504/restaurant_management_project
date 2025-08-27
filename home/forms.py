b2dee3d908dcca3f2be1ceb2a065693bd1e16a10
sriram11200504 <sriramlohith25@gmail.com>
2025-08-27 22:20:57 +0530

    39255

--- home/forms.py
+++ home/forms.py
@@ -1,8 +1,8 @@
 from django import forms
 
 class ContactForm(forms.Form):
-    name=forms.CharField(max_length=100,required=False)
-    email=forms.EmailField(required=True)
-    subject=forms.CharField(required=False,max_length=200)
-    message=forms.CharField(width=forms.Textarea,required=True)
+    name=forms.CharField(max_length=100,required=False,help_text="your name")
+    email=forms.EmailField(required=True,help_text="your email")
+    subject=forms.CharField(required=False,max_length=200,help_text="your subject")
+    message=forms.CharField(width=forms.Textarea,required=True,help_text="your message")
     
