a9e70269b84bef344c819f96a3c635d818dab14c
sriram11200504 <sriramlohith25@gmail.com>
2025-09-03 22:12:56 +0530

    41635

--- home/forms.py
+++ home/forms.py
@@ -1,8 +1,13 @@
 from django import forms
+from .models import newslettersubscriber
 
 class ContactForm(forms.Form):
     name=forms.CharField(max_length=100,required=False,help_text="your name")
     email=forms.EmailField(required=True,help_text="your email")
     subject=forms.CharField(required=False,max_length=200,help_text="your subject")
     message=forms.CharField(width=forms.Textarea,required=True,help_text="your message")
+class newsletterform(forms.ModelForm):
+    class Meta:
+        model=newslettersubscriber
+        fields=['email']
     
