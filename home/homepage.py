1094b2193df62c37e9259a7c49a3ad1c883ff106
sriram11200504 <sriramlohith25@gmail.com>
2025-09-03 22:01:39 +0530

    41638

--- home/homepage.py
+++ home/homepage.py
@@ -67,7 +67,7 @@ urlpatterns = [
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Restaurant Menu</title>
+    <title>HomeFoods</title>
     <script src="[https://cdn.tailwindcss.com](https://cdn.tailwindcss.com)"></script>
     <style>
         @import url('[https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap](https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap)');
