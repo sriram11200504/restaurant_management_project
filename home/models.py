acced60a31fc84815217cec0ed66d5b4a4701107
sriram11200504 <sriramlohith25@gmail.com>
2025-09-03 22:10:13 +0530

    41635

--- home/models.py
+++ home/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class newslettersubscriber(models.Model):
+    email=models.EmailField(unique=True,help_text="Enter you email to subscribe")
+    created_at=models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.email
