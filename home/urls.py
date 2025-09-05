d635011a5af4713ba3b2a63e4bd93298bd16db40
sriram11200504 <sriramlohith25@gmail.com>
2025-09-05 11:39:01 +0530

    42646

--- home/urls.py
+++ home/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('reservation/',views.reservations_view,name="reservation"),
     path('feedback_form/',views.feedback_form_view,name="feedback"),
     path('menu/',views.menu_page_view,name="menu_page"),
-    path('contact_form',)
+    path('contact',views.contact_view,name="contact")
 ]
