1c24b01cb71eb17c6c1c3e2e422e3cb313a3d1c3
sriram11200504 <sriramlohith25@gmail.com>
2025-09-05 11:35:32 +0530

    42646

--- home/urls.py
+++ home/urls.py
@@ -4,5 +4,6 @@ from .views import *
 urlpatterns = [
     path('about/',views.about_view,name="about"),
     path('our_story/',views.our_story_view,name="our_story"),
-    path()
+    path('reservation/',views.reservations_view,name="reservation"),
+    
 ]
